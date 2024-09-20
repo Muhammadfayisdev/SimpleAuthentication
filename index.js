4fcfeca26ef2f17b25e18ffb62d4b56b46be0508
Muhammad Fayis <fayismuhammadp@gmail.com>
2024-09-20 21:14:23 +0530

    changed token implementation by using jwt

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 const express=require('express');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET='JWT_SECRET';
+
 const app=express();
 
 app.use(express.json());
 
 const users=[];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() {
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-   let token="";
-   for(let i=0;i<32;i++){
-        token+=options[Math.floor(Math.random() * options.length)]
-   }
-   return token;
-}
+//    let token="";
+//    for(let i=0;i<32;i++){
+//         token+=options[Math.floor(Math.random() * options.length)]
+//    }
+//    return token;
+// }
 
 //signin endpoint
 
@@ -24,7 +27,8 @@ app.post('/signin',function(req,res){
 
     let user=users.find(user=>user.username===username&&user.password===password);
     if(user){
-        const token=generateToken();
+        // const token=generateToken();
+        const token=jwt.sign({username:username},JWT_SECRET);
         user.token=token;
         res.json({
             message:'User successfully signed in',
@@ -76,7 +80,10 @@ app.post('/signup',function(req,res){
 
 app.post('/me',function(req,res){
     const token=req.headers.token;
-    const user=users.find(user=>user.token===token);
+    const userdetails=jwt.verify(token,JWT_SECRET);
+    const username=userdetails.username;
+
+    const user=users.find(user=>user.username===username);
     if(user){
         res.json({
             message:'Authorized user',
